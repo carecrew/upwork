1876a1760bf3e71aeae11356f8d285c4f9857518
linux08 <abimbola130@gmail.com>
2019-09-25 21:30:26 +0100

    #3- Added Screen-1 component

--- app/screens/screen1/screen1.js
+++ app/screens/screen1/screen1.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View } from 'react-native';
-
 import Screen1Logic from './screen1.logic';
 import Screen1Style from './screen1.style';
 import Screen1Data from './screen1.data';
 
 const Screen1 = () => (
-  <View />
-  // fill in here
+  <Screen1Data
+    toLogic={currentData => (
+      <Screen1Logic
+        currentData={() => ({ ...currentData })}
+        toStyle={({ logic, state }) => <Screen1Style logic={logic} state={state} />}
+      />
+    )}
+  />
 );
 
 export default Screen1;
